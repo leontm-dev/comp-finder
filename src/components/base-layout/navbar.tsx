9b9f41c4b4b49c8281202c764471292f8e1c0abc
Leon <LAnneken09@gmail.com>
2026-06-22 17:49:06 +0200

    Added some more props to the navbar, made some size and color changes as well as some layout adjustments

--- src/components/base-layout/navbar.tsx
+++ src/components/base-layout/navbar.tsx
@@ -1,18 +1,24 @@
 import { Search, Star } from "lucide-react";
-import { Button } from "../ui/button";
-import { cn } from "@/lib/utils";
+import { Button, buttonVariants } from "../ui/button";
+import cn from "cnfast";
 import { Black_Ops_One } from "next/font/google";
 import Link from "next/link";
+import { VariantProps } from "class-variance-authority";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { GithubIcon } from "@hugeicons/core-free-icons";
+import { ModeToggle } from "../mode-toggle";
 
 type Props = {
   className?: HTMLDivElement["className"];
+  buttonVariant?: VariantProps<typeof buttonVariants>["variant"];
+  showToggle?: boolean;
 };
 const blackOpsOne = Black_Ops_One({ weight: "400" });
 export function Navbar(props: Props) {
   return (
     <nav
       className={cn(
-        "flex w-full flex-row items-center justify-between px-2 py-4",
+        "flex w-full flex-row items-center justify-between px-4 py-4",
         props.className,
       )}
     >
@@ -20,13 +26,14 @@ export function Navbar(props: Props) {
         Comb finder
       </h2>
       <div className="flex flex-row items-center gap-2">
-        <Link href={"https://github.com"}>
-          <Button variant={"secondary"}>
-            <Star />
+        {props.showToggle && <ModeToggle />}
+        <Link href={"https://github.com/leontm-dev/comb-finder-frontend"}>
+          <Button variant={"ghost"} size={"icon-lg"}>
+            <HugeiconsIcon icon={GithubIcon} />
           </Button>
         </Link>
         <Link href={"/find"}>
-          <Button>
+          <Button variant={props.buttonVariant || "default"} size={"lg"}>
             <Search /> Find
           </Button>
         </Link>
