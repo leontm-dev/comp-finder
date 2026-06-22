f68e80d094b19ca422d227da6672696c008cf0c7
Leon <LAnneken09@gmail.com>
2026-06-22 21:03:24 +0200

    Showing toggle on the trending page now too

--- src/app/trending/page.tsx
+++ src/app/trending/page.tsx
@@ -8,7 +8,7 @@ export default function TrendingPage() {
   return (
     <div className="relative flex max-w-screen flex-col">
       <main className="flex w-full flex-col items-center">
-        <Navbar />
+        <Navbar showToggle />
         <div className="relative flex h-full w-full grow-0 flex-col px-2 pb-4 md:px-4 lg:px-6 xl:px-8">
           <Suspense fallback={<Skeleton className="h-full w-full flex-1" />}>
             <TrendingCustomizerForm />
